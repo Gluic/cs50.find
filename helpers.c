484400f0fa13fbccf81640027e1ed97545cc510c
Gluic <gluic.hp@gmail.com>
2016-05-21 12:18:32 -0400

    second

--- helpers.c
+++ helpers.c
@@ -17,9 +17,13 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+    int x = 0;
     if (n <=0)
         return false;
     else
+        while (x )
+        {
+        
         for (int i = 0; i < n; i++)
             {
                 if (value == values[i])
@@ -34,5 +38,24 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    int temp;
+    int count = 0;
+    while (n != 0 || count == n - 1)
+    {
+        count = 0;
+        for (int i = 0; i < n-1; i++)
+        {
+            if (values[i] > values [i+1])
+            {
+                temp = values [i];
+                values [i] = values [i+1];
+                values [i+1] = temp;
+            }
+            else
+                count = count + 1;
+        }
+        n = n - 1;   
+     }
+    
     return;
 }
